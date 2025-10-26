c228bb1559b11a2371138b274703ddb863ce1960
MartuZeman <marty-z97@live.com>
2025-10-26 12:36:45 -0300

    cambios errores

--- src/components/CartContainer.jsx
+++ src/components/CartContainer.jsx
@@ -1,61 +1,66 @@
-import cartContext from "../context/cartContext";
+import cartContext from "../context/cartContext"
 import { useContext } from "react";
 import { Link } from "react-router";
 import { createBuyOrder } from "../data/firestore";
-import FormCheckout from "./formCheckOut";
+import FormCheckout from "./formCheckOut"
 
-function CartContainer() {
-  const { cart, removeItem, clearCart } = useContext(cartContext);
-  const total = cart.reduce((acc, item) => acc + item.price * item.count, 0);
+function CartContainer(){
 
-  async function handleCheckout(formData) {
-    const buyOrder = {
-      buyer: formData,
-      cart: cart,
-      date: new Date(),
-      total: total,
-    };
-    const orderDocument = await createBuyOrder(buyOrder);
-    console.log(orderDocument);
-    clearCart();
-    alert(`Hiciste tu compra! el id de tu compra es: ${orderDocument.id} `);
-  }
+    const {cart, removeItem, clearCart} = useContext(cartContext)
+    const total = cart.reduce((acc, item) => acc + item.price * item.count, 0);
 
-  if (cart.length === 0) {
-    return (
-      <div>
-        <h2>Tu carrito esta vacio</h2>
-        <Link to="/">
-          <button>Volver al inicio</button>
-        </Link>
-      </div>
-    );
-  }
-  return (
-    <section>
-      <h2>Tu carrito de compras</h2>
-      <div>
-        <ul>
-          {cart.map((itemInCart) => (
-            <div key={itemInCart.id}>
-              <h4>{itemInCart.title}</h4>
-              <p>Price: {itemInCart.price}</p>
-              <p> Cantidad: {itemInCart.count}</p>
-              <button onClick={() => removeItem(itemInCart.id)}>🗑️</button>
+
+   async function handleCheckout(formData){
+        const buyOrder = {
+            buyer: formData,
+            cart: cart,
+            date: new Date(),
+            total: total,
+        }
+        const orderDocument = await createBuyOrder(buyOrder)
+        console.log(orderDocument)
+        clearCart()
+        alert(`Hiciste tu compra! el id de tu compra es: ${orderDocument.id} `)
+    }
+
+    if(cart.length === 0 ){
+         return(
+            <div>
+                <h2>Tu carrito esta vacio</h2>
+                <Link to="/">
+                <button>Volver al inicio</button>
+                </Link>
             </div>
-          ))}
-        </ul>
-        <button onClick={() => clearCart()}> Vaciar Carrito </button>
-      </div>
+         )
+    }
+    return(
+        <section>
+            < h2>
+               Tu carrito de compras 
+            </h2>
+            <div>
+            <ul>
+                {cart.map (itemInCart => <div key={itemInCart.id}>
+                    <h4>{itemInCart.title}</h4>
+                    <p>Price: {itemInCart.price}</p>
+                    <p> Cantidad: {itemInCart.count}</p>
+                    <button onClick={()=> removeItem(itemInCart.id)}>🗑️</button>
+                </div>)
+                }
+            </ul>
+            <button onClick={()=>clearCart()}> Vaciar Carrito </button>
+            </div>
 
-      <hr />
-      <div>
-        <h3> Total de tu compra: ${total}</h3>
-      </div>
-      {/* <button onClick={handlePayment}>Pagar</button> */}
-      <FormCheckout handleCheckout={handleCheckout} />
-    </section>
-  );
+            <hr />
+            <div>
+                <h3> Total de tu compra: ${total}</h3>
+            </div>
+            {/* <button onClick={handlePayment}>Pagar</button> */}
+            <FormCheckout handleCheckout={handleCheckout}/>
+
+        </section>
+    )
+
 }
 
 export default CartContainer;
